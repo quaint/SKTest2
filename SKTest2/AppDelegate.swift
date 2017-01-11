77f890c76e86505065017f6f56b6f6ab6571275f
Tomasz Wiśniewski <tomasz.wisniewski@globallogic.com>
2017-01-11 12:33:57 +0100

    Initial Commit

---
+++ SKTest2/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SKTest2
+//
+//  Created by Tomasz Wiśniewski on 11/01/2017.
+//  Copyright © 2017 GlobalLogic. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ SKTest2/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SKTest2
+//
+//  Created by Tomasz Wiśniewski on 11/01/2017.
+//  Copyright © 2017 GlobalLogic. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
